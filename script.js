59d0b1eeca9166fc8f3c9a6d7cdb00022294667b
ngotfryd123 <nick_gotfryd@hotmail.com>
2023-03-15 09:55:00 -0700

    Removed row divs

--- script.js
+++ script.js
@@ -3,19 +3,14 @@ function createGrid(x,y){
 const container = document.querySelector('#container');
 let grid = document.createElement('table');
 
-    for (let i=0;i<x;i++){
-        let rowDivs = document.createElement('div');
-        rowDivs.textContent="x";
-        rowDivs.setAttribute('style', 'color: blue; background: red;');    
+    for (let i=0;i<x;i++){   
         container.appendChild(grid);
         let row = grid.insertRow(i);
-        row.appendChild(rowDivs);
-
+        
             for (let i=0; i<y; i++){
-            if (i===(y-1)){break;}
             let cell = row.insertCell(i);
             let cellDivs = document.createElement('div');
-            cellDivs.textContent="y";
+            cellDivs.textContent="x";
             cellDivs.setAttribute('style', 'color: blue; background: red;'); 
             cell.appendChild(cellDivs);}
     }
