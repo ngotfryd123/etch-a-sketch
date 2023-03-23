d5dd05a51f22bceeab3fac155e58d14e254aa951
ngotfryd123 <nick_gotfryd@hotmail.com>
2023-03-22 22:11:46 -0700

    Added random color and opacity

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
 const container = document.querySelector('#container');
 
-function createGrid(x,y){
+function createGrid(x,y,){
 
 let grid = document.createElement('table');
+container.appendChild(grid);
 
     for (let i=0;i<x;i++){   
-        container.appendChild(grid);
+        
         let row = grid.insertRow(i);
         
             for (let i=0; i<y; i++){
             let cell = row.insertCell(i);
             let cellDivs = document.createElement('div');
             cellDivs.setAttribute('style', 'background: white;');
-            cellDivs.addEventListener("mouseover", (event) => {cellDivs.style.background = "black";});
-            cell.appendChild(cellDivs);}
-    }
-}
-function eraseChildren(element){
+            cellDivs.addEventListener("mouseover", changeColor);
+            cell.appendChild(cellDivs);}}}
+            
+function clearGrid(element){
     element.replaceChildren();
-    let ask = prompt("Enter a number up to 100 to set up a new grid: ");
-    createGrid(ask,ask);
-}
+    ask = prompt("Enter a number (1-100) to set up a new grid: ");
+    while (ask <=0 || ask >100){
+        ask=prompt("Enter a number (1-100): ");}
+    createGrid(ask,ask);}
 
-let num1=10;
-let num2=10;
-    createGrid(num1,num2);
+function randomRGB() {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let RGBColor = "rgb(" + x + "," + y + "," + z + ")";
+    return RGBColor}
+ 
+function changeColor(){
+    if (color==="random"){
+        this.style.background=randomRGB();}
+    else if (color==="opaque"){
+        this.style.backgroundColor="black";
+        this.style.opacity =(parseFloat(this.style.opacity) || 0) + 0.1;}
+    else this.style.backgroundColor = color;}
+
+function userChoice(choice){
+    color = choice;}
+
+let ask=10;
+let color="black";
+
+createGrid(ask,ask);
     
 
